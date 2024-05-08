41c829aa1e1b3feb253dd71aeea7096911d66aeb
KimTaeGang <tgkim@enliple.com>
2024-05-08 18:59:59 +0900

    test 코드 작성

--- src/test/java/com/study/module/UserApiControllerTest.java
+++ src/test/java/com/study/module/UserApiControllerTest.java
@@ -3,27 +3,38 @@ package com.study.module;
 
 import com.study.RestDocumentConfig;
 import com.study.module.user.adapter.input.UserApiController;
+import com.study.module.user.application.port.input.UserFindQuery;
+import com.study.module.user.domain.UserDomain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
 
+import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = UserApiController.class)
 @AutoConfigureRestDocs
 public class UserApiControllerTest extends RestDocumentConfig {
 
-    private static String signUp = "/user/signUp";
+    private static String signUp = "/api/v1/user/signUp";
+    private static String findUser = "/api/v1/user/{userId}";
+
+    @MockBean private UserFindQuery userFindQuery;
     @Test
     @DisplayName("회원 가입")
     void register() throws Exception {
         //given
+
 
         //when
         ResultActions actions = mockMvc.perform(
@@ -40,5 +51,35 @@ public class UserApiControllerTest extends RestDocumentConfig {
                 )
 
         ).andExpect(status().isCreated());
+    }
+
+    @Test
+    @DisplayName("회원 정보 조회")
+    void findByUserId() throws Exception {
+        UserDomain response = new UserDomain(
+                "test13",
+                "test13",
+                "test13@naver.com"
+        );
+
+        //given
+        given(userFindQuery.findOne("test13")).willReturn(response);
+
+        //when
+        ResultActions actions = mockMvc.perform(
+                get(findUser, "test13")
+        );
+
+        //then
+        actions.andDo(
+                document(
+                        "{class-name}/{method-name}",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("userId").description("사용자 아이디")
+                        )
+                )
+        ).andExpect(status().isOk());
     }
 }
