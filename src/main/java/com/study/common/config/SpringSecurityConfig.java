b348b78ff62b3dae6289ff7e62c8d58c5c212246
KIMTAEGANG <64457893+KIMTAEGANG@users.noreply.github.com>
2024-06-16 17:37:59 +0900

    spring security 설정 작업중 #2

--- src/main/java/com/study/common/config/SpringSecurityConfig.java
+++ src/main/java/com/study/common/config/SpringSecurityConfig.java
@@ -1,41 +1,67 @@
 package com.study.common.config;
 
+import com.study.module.user.application.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.stereotype.Component;
 
 
-@Component
+@Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SpringSecurityConfig {
+    private final UserService userService;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         authorizeRequest ->
                                 authorizeRequest.requestMatchers(
-                                        AntPathRequestMatcher.antMatcher("/auth/**")
-
-                                ).authenticated()
-                                        .requestMatchers("/**").permitAll()
+                                        "/api/v1/user/signUp", "api/v1/user/login"
+                                ).permitAll()
+                                        .anyRequest().authenticated()
                 )
-                .headers(
-                        headersConfigurer ->
-                                headersConfigurer
-                                        .frameOptions(
-                                                HeadersConfigurer.FrameOptionsConfig::sameOrigin
-                                        )
+                .logout(logout ->
+                    logout.logoutSuccessUrl("/login")
+                            .invalidateHttpSession(true)
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 );
+//        http
+//                .addFilterAfter()
+//                .addFilterBefore()
         return http.build();
+    }
+
+    //인증 관리자
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
 }
